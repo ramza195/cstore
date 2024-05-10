e33a0664eef23bdb348ebef1df9c8d2bd78af020
ramza195 <166901096+ramza195@users.noreply.github.com>
2024-05-10 23:09:43 +0300

    stergere parola email

--- cstore/settings.py
+++ cstore/settings.py
@@ -139,6 +139,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'no.reply.c.store@gmail.com'
-EMAIL_HOST_PASSWORD = 'culu urut wyuh qyey'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 DEFAULT_FROM_EMAIL = 'no.reply.c.store@gmail.com'
